cce71fcdbb5a9f9f84713f5483583f7e6cc9038a
luo216 <3337990267@qq.com>
2023-05-22 13:33:06 +0800

    browser firefox

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,5 +3,9 @@ return {
 		"iamcco/markdown-preview.nvim",
 		ft = "markdown",
     build = "cd app && yarn install",
+    -- 设置preveiew浏览器
+    config = function()
+      vim.g.mkdp_browser = "firefox"
+    end,
 	},
 }
