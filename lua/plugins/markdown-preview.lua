0ddfe4d74cd491b58863372c5536812ce045ef70
luo216 <3337990267@qq.com>
2023-05-22 11:17:54 +0800

    markdown-preview build

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,8 +2,6 @@ return {
 	{
 		"iamcco/markdown-preview.nvim",
 		ft = "markdown",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+    build = "cd app && yarn install",
 	},
 }
--- setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd ~/.local/share/nvim/lazy/coc.nvim && yarn install && cd ~/.local/share/nvim/lazy/markdown-preview.nvim/app && yarn install
