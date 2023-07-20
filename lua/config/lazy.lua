4b56e3023253f8af8511bc5a8849d35488ae40cb
steve <333799267@qq.com>
2023-07-20 15:49:54 +0800

    add plugin golang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
